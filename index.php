1f7d839cea64ef60502c15a6f43a8e4e30743b3e
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 17:33:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $client_id = "Client-".rand();
-  echo "Start\n";
+  echo "Start1\n";
 $mqtt = new phpMQTT($server, $port, $client_id);
 if( !$mqtt->connect(true, NULL, $username, $password) ) {
  exit(1);
 }
 //$topics['test/topic'] = array("qos" => 0, "function" => "procmsg");
-//$topics['led'] = array("qos" => 0, "function" => "procmsg");
-//$mqtt->subscribe($topics, 0);
-
+$topics['led'] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+echo "Start2\n";
 //while($mqtt->proc()){
 
 //}
