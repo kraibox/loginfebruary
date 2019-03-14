e57d7228679d871d19c7ae8457c3f655c1bfc6a6
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 17:01:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,18 @@ $username = "vuwseiaf";  //username ที่ได้สร้างไว้�
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
  $client_id = "Client-".rand();
 
-$mqtt = new phpMQTT($server, $port, $client_id);
-
-if ($mqtt->connect(true, NULL, $username, $password)) {
- $mqtt->publish("led", "Hello World! This is message from publisher.", 0);
- $mqtt->close();
-} else {
-    echo "Time out!\n";
+ $mqtt = new phpMQTT($server, $port, $client_id);
+if( !$mqtt->connect(true, NULL, $username, $password) ) {
+ exit(1);
 }
-
+$topics['test/topic'] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+while($mqtt->proc()){
+}
+$mqtt->close();
+function procmsg($topic, $msg){
+  echo "Recieved at: " . date("Y-m-d H:i:s", time()) . "\n";
+  echo "Topic: {$topic}\n";
+  echo "Message: $msg\n\n";
+}
 ?>
