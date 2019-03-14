b82561eb690128e543b8f2527c49b1a73db5c2a1
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 20:47:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,10 @@ exit(1);
 
 $topics[‘led’] = array(“qos”=>0, “function”=>”procmsg”);
 $mqtt->subscribe($topics,0);
-while($mqtt->proc()){
-}
-$mqtt->close();
+echo “okok: $msg”.”\r\n”;
+//while($mqtt->proc()){
+//}
+//$mqtt->close();
 function procmsg($topic,$msg){
 echo “Msg Recieved: $msg”.”\r\n”;
 }
