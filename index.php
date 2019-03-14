473907f752e81abfdc64dd63a2b3ed82d9267d91
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 17:59:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,17 @@ if( !$mqtt->connect(true, NULL, $username, $password) ) {
  exit(1);
 }
 //$topics['test/topic'] = array("qos" => 0, "function" => "procmsg");
+ echo "Start2\n\n";
 $topics['led'] = array("qos" => 0, "function" => "procmsg");
+ echo "Start3\n\n";
 $mqtt->subscribe($topics, 0);
+ echo "Start4\n\n";
+//procmsg($topic, $msg);
+//while($mqtt->proc()){
 
-while($mqtt->proc()){
+//}
 
-}
-
-$mqtt->close();
+//$mqtt->close();
 
 function procmsg($topic, $msg){
   echo "Recieved at: " . date("Y-m-d H:i:s", time()) . "\n";
