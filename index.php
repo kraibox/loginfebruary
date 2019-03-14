805673f15aaa6614ad8d163ddb0b6ba56b13b44e
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 16:57:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,22 @@
 <?php
-
 require("phpMQTT.php");
-
 $server = "m16.cloudmqtt.com"; //"m16.cloudmqtt.com" ;     // change if necessary
 $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $client_id = "Client-".rand();
-
 $mqtt = new phpMQTT($server, $port, $client_id);
 if( !$mqtt->connect(true, NULL, $username, $password) ) {
  exit(1);
 }
-
 $topics['led'] = array("qos" => 0, "function" => "procmsg");
 $mqtt->subscribe($topics, 0);
-
 while($mqtt->proc()){
-
 }
-
 $mqtt->close();
-
 function procmsg($topic, $msg){
   echo "Recieved at: " . date("Y-m-d H:i:s", time()) . "\n";
   echo "Topic: {$topic}\n";
   echo "Message: $msg\n\n";
 }
-
 ?>
