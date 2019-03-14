f67cf3f5cd013a63c13feeba0470e8149e1feb95
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-14 17:23:40 +0700

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -7,7 +7,7 @@ $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $client_id = "Client-".rand();
-
+  echo "Start\n";
 $mqtt = new phpMQTT($server, $port, $client_id);
 if( !$mqtt->connect(true, NULL, $username, $password) ) {
  exit(1);
