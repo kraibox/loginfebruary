c366e175b5375ce78ce5b92d95b2b4ab8f727a4e
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-15 13:32:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 
-require("phpMQTT.php");
-
-$server  = "m16.cloudmqtt.com";
-$port  = 14303;
-$username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
-$password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
- $client_id = "Client-".rand();
-
-$mqtt = new phpMQTT($server, $port, $client_id);
-
-if ($mqtt->connect(true, NULL, $username, $password)) {
- $mqtt->publish("led", "Hello World! This is message from publisher.", 0);
- $mqtt->close();
-} else {
-    echo "Time out!\n";
+require "phpMQTT.php";
+$mqtt = new phpMQTT("m16.cloudmqtt.com", 14303, "Emoncms feed subscriber");
+if (!$mqtt->connect()) {
+    exit(1);
 }
-echo "Finish Publish\n";
-?>
+$topics["emoncms/#"] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+while ($mqtt->proc()) {
+}
+$mqtt->close();
+function procmsg($topic, $value)
+{
+    $time = time();
+    print $topic . " " . $value . "\n";
+}
