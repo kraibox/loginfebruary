74801597d93f886eb5d0edb6a77e14bc807b14cf
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-15 14:36:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-
 require "phpMQTT.php";
-$mqtt = new phpMQTT("m16.cloudmqtt.com", 14303, "Emoncms feed subscriber");
+  echo "Connect11\n\n";
+
+  $client_id = "Client-".rand();
+
+$mqtt = new phpMQTT("m16.cloudmqtt.com", 14303, $client_id);
 if (!$mqtt->connect()) {
     exit(1);
 }
-$topics["emoncms/#"] = array("qos" => 0, "function" => "procmsg");
+echo "topics\n\n";
+$topics["led"] = array("qos" => 0, "function" => "procmsg");
 $mqtt->subscribe($topics, 0);
 while ($mqtt->proc()) {
 }
