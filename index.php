52850e9d560efbe1e9696e2ce0c8a0cfabf80433
kraibox <47628647+kraibox@users.noreply.github.com>
2019-03-19 21:16:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,28 +9,31 @@ $server = "m16.cloudmqtt.com"; //"m16.cloudmqtt.com" ;     // change if necessar
 $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
-$client_id =   "vuwseiaf";    //"Client-".rand();
-  echo "Start login\n\n";
+$client_id = "Client-".rand();
+ 
 $mqtt = new phpMQTT($server, $port, $client_id);
  echo "connecting!\n\n";
-if( !$mqtt->connect(true, NULL, $username, $password) ) {
-  echo "Fail\n\n";
- exit(1);
-}
-echo "Connected.. Start subscribe1\n : $query :\n";
-  $topics[$query] = array("qos" => 0, "function" => "procmsg");
-
-$mqtt->subscribe($topics,0);
-echo ": subscribe query OK...........\n\n";
-
-
-//while ($mqtt->proc()) {
+//if( !$mqtt->connect(true, NULL, $username, $password) ) {
+//  echo "Fail\n\n";
+// exit(1);
 //}
-while($mqtt->proc()) {}
+//echo "Connected.. Start subscribe1\n : $query :\n";
+//  $topics[$query] = array("qos" => 0, "function" => "procmsg");
+//$mqtt->subscribe($topics,0);
+//echo ": subscribe query OK...........\n\n";
+if ($mqtt->connect(true, NULL, $username, $password)) {
+  $topics[$query] = array("qos" => 0,"function" => "procmsg");
+  $mqtt->subscribe($topics,0);
+  while($mqtt->proc()) {}
   $mqtt->close();
 } else {
   exit(1);
 }
+
+
+//while ($mqtt->proc()) {
+//}
+ 
 
 
 
