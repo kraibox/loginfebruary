4b71ddf284504aa356ce01cbb24e9b6c76f0ce70
kraibox <47628647+kraibox@users.noreply.github.com>
2019-04-03 20:35:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,25 +2,28 @@
 
 require("phpMQTT.php");
 
-$server  = "m16.cloudmqtt.com";
+$server = "m16.cloudmqtt.com"; //"m16.cloudmqtt.com" ;     // change if necessary
 $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
- $client_id = "Client-".rand();
-
+$client_id = "Client-".rand();
+  echo "Start\n";
 $mqtt = new phpMQTT($server, $port, $client_id);
-
-if ($mqtt->connect(true, NULL, $username, $password)) {
- $mqtt->publish("API1", "88", 0);
-sleep(5);
- $mqtt->publish("API1", "22", 0);
- sleep(5);
- $mqtt->publish("API1", "38", 0);
-sleep(5);
- $mqtt->publish("API1", "72", 0);
- $mqtt->close();
-} else {
-    echo "Time out!\n";
+if( !$mqtt->connect(true, NULL, $username, $password) ) {
+ exit(1);
 }
-echo "Finish Publish\n";
+//$topics['test/topic'] = array("qos" => 0, "function" => "procmsg");
+$topics['API1'] = array("qos" => 0, "function" => "procmsg");
+$mqtt->subscribe($topics, 0);
+
+//while($mqtt->proc()){}
+sleep(10);
+$mqtt->close();
+
+function procmsg($topic, $msg){
+  echo "Recieved at: " . date("Y-m-d H:i:s", time()) . "\n";
+  echo "Topic: {$topic}\n";
+  echo "Message: $msg\n\n";
+}
+
 ?>
