17e9c930d18fcc39ecae8aea1f2550dddfb92649
kraibox <47628647+kraibox@users.noreply.github.com>
2019-05-07 22:24:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $port  = 14303;
 $username = "vuwseiaf";  //username ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
 $password = "qyHizNCHb3a3";  //password ที่ได้สร้างไว้ตอนตั้งค่า MQTT Broker
  $client_id = "Client-".rand();
+While(1){
 $mqtt = new phpMQTT($server, $port, $client_id);
  
 if ($mqtt->connect(true, NULL, $username, $password)) {
@@ -24,5 +25,5 @@ echo "ok\n";
     echo "Time out!\n";
 }
 echo "Finish Publish\n";
-
+}
 ?>
